b4261a12a98c9d5f32574e66cc8ddf2cea812b76
Marcelo <msr88957@gmail.com>
2024-03-30 14:43:22 -0300

    repository and services

--- messages/src/messages/messages.service.ts
+++ messages/src/messages/messages.service.ts
@@ -1,3 +1,26 @@
+import { MessagesRepository } from "./messages.repository"
+
+export class MessagesService {
+
+    private messageRepo: MessagesRepository;
+
+    constructor() {
+        // Service is createing its own dependencies
+        // DONT DO THIS ON REAL APPS
+        this.messageRepo = new MessagesRepository()
+    }
+
+    async findOne(id: string) {
+        return this.messageRepo.findOne(id)
+    }
+
+    async findAll() {
+        return this.messageRepo.findAll()
+    }
+
+    create(content: string) {
+        return this.messageRepo.create(content)
+    }
 
 
-
+}
