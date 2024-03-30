457325535a49f18c145ff50931f46581787dd183
Marcelo <msr88957@gmail.com>
2024-03-29 21:10:56 -0300

    first commit

---
+++ scratch/src/app.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Module, Get } from '@nestjs/common';
+
+
+@Controller('/app')
+export class AppController {
+
+    @Get('/asdf')
+    getRootRoute() {
+        return 'hi there!'
+    }
+
+    @Get('/bye')
+    getByeThere() {
+        return 'bye there!'
+    }
+
+}
