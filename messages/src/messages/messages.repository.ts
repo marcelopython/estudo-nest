9192045f9a942bdf3d6d2c4e7a9a69ef10355e90
Marcelo <msr88957@gmail.com>
2024-03-29 22:24:22 -0300

    repository messages

---
+++ messages/src/messages/messages.repository.ts
@@ -0,0 +1,33 @@
+import { readFile, writeFile } from "fs/promises";
+
+
+export class MessagesRepository {
+    
+    async findOne(id: string) {
+
+        const messages = this.getAllJson()
+        return messages[id]
+
+    }
+
+    async findAll() {
+        return this.getAllJson()
+    }
+
+    async create(content: string) {
+
+        const messages = this.getAllJson()
+
+        const id = Math.floor(Math.random() * 999)
+
+        messages[id] = {id, content}
+
+        await writeFile('messages.json', JSON.stringify(messages))
+
+    }
+
+    async getAllJson() {
+        const contents = await readFile('messages.json', 'utf8')
+        return JSON.parse(contents)
+    }
+}
