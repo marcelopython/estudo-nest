e9830712a15ada8c8b9beaa369baef3036d66ddc
Marcelo <msr88957@gmail.com>
2024-03-31 22:49:44 -0300

    messages

--- messages/src/messages/messages.controller.ts
+++ messages/src/messages/messages.controller.ts
@@ -5,7 +5,9 @@ import { MessagesService } from './messages.service';
 @Controller('messages')
 export class MessagesController {
 
-    constructor(public messagesService: MessagesService) { }
+    constructor(
+        public messagesService: MessagesService,
+    ) {     }
 
     @Get()
     listMessages() {
